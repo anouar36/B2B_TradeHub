1bdadc830ed1b9a7044652162f316121fd66287f
anouar <anouarechcharai@gmail.com>
2025-11-24 17:04:33 +0100

    first commit

---
+++ src/main/java/com/logitrack/b2b_tradehub/B2BTradeHubApplication.java
@@ -0,0 +1,13 @@
+package com.logitrack.b2b_tradehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2BTradeHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B2BTradeHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logitrack/b2b_tradehub/B2BTradeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logitrack.b2b_tradehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2BTradeHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
